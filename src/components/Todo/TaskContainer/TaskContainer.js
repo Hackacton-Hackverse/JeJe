5ca0cabc50dedddfefceb1dfb4b5f69f0df9df72
[nom] <134686610+wel1s@users.noreply.github.com>
2024-03-30 23:32:45 +0100

    ajout de la logique de sauvegarde dans le navigateur

--- src/components/Todo/TaskContainer/TaskContainer.js
+++ src/components/Todo/TaskContainer/TaskContainer.js
@@ -1,36 +1,47 @@
-// TaskContainer.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from '../TaskList/TaskList';
 
 const TaskContainer = () => {
-    const [taskLists, setTaskLists] = useState([]);
-  
-    const addTaskList = () => {
-      const newTaskList = {
-        idL: Date.now(),
-        test:"toto",
-      };
-      setTaskLists([...taskLists, newTaskList]);
-      // setTaskLists([...taskLists, []]);
+  const [taskLists, setTaskLists] = useState([]);
+
+  // Chargez les données depuis localStorage lors du chargement initial
+  useEffect(() => {
+    const savedData = localStorage.getItem('taskLists');
+    if (savedData) {
+      setTaskLists(JSON.parse(savedData));
+    }
+  }, []);
+
+  const addTaskList = () => {
+    const newTaskList = {
+      idL: Date.now(),
+      test: "toto",
     };
-    const deleteTaskList1 = (idList) => {
-      setTaskLists(taskLists.filter((taskList) => taskList.idL !== idList));
-    };
-    return (
-      <div className={`flex flex-wrap justify-center gap-4 p-5 overflow-x-auto ${taskLists.length > 2 ? 'overflow-x-scroll' : ''}`}>
-        <button
-          className="w-full mt-2 p-2.5 text-white bg-blue-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
-          onClick={addTaskList}
-        >
-          Ajouter une TaskList 
-        </button>
-        {taskLists.map((taskList, index) => (
-          <TaskList key={index} idList={taskList.idL} onDeleteList={deleteTaskList1} />  
-        ))}
-        
-      </div>
-    );
+    setTaskLists([...taskLists, newTaskList]);
+  };
+
+  const deleteTaskList1 = (idList) => {
+    setTaskLists(taskLists.filter((taskList) => taskList.idL !== idList));
+  };
+
+  // Sauvegardez les données dans localStorage à chaque modification
+  useEffect(() => {
+    localStorage.setItem('taskLists', JSON.stringify(taskLists));
+  }, [taskLists]);
+
+  return (
+    <div className={`flex flex-wrap justify-center gap-4 p-5 overflow-x-auto `}>
+      <button
+        className="w-full mt-2 p-2.5 text-white bg-blue-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
+        onClick={addTaskList}
+      >
+        Ajouter une TaskList
+      </button>
+      {taskLists.map((taskList, index) => (
+        <TaskList key={index} idList={taskList.idL} onDeleteList={deleteTaskList1} />
+      ))}
+    </div>
+  );
 };
-  
 
 export default TaskContainer;
--- src/components/Todo/TaskList/TaskList.js
+++ src/components/Todo/TaskList/TaskList.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskItem from '../TaskItem/TaskItem';
 import AddTaskModal from '../TaskModal/AddTaskModal1'; // Importez le composant AddTaskModal
 import { FaTimes } from 'react-icons/fa'; // Importez l'icône de fermeture
 import DeleteTaskModalList from '../TaskModal/DeleteTaskModalList'; // Importez le composant DeleteTaskModal pour la suppression
 
-const TaskList = ({onDeleteList,idList}) => {
+const TaskList = ({ onDeleteList, idList }) => {
   const [tasks, setTasks] = useState([]);
-  const [showAddModal, setShowAddModal] = useState(false); // État pour afficher ou masquer le modal d'ajout
+  const [showAddModal, setShowAddModal] = useState(false);
   const [showDeleteModalList, setShowDeleteModalList] = useState(false);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem(`taskList_${idList}`);
+    if (savedData) {
+      setTasks(JSON.parse(savedData));
+    }
+  }, [idList]);
 
   const addTask = (text) => {
     const newTask = {
@@ -15,27 +22,31 @@ const TaskList = ({onDeleteList,idList}) => {
       text,
     };
     setTasks([...tasks, newTask]);
-    setShowAddModal(false); // Fermez le modal après avoir ajouté la tâche
+    setShowAddModal(false);
   };
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
   const deleteTaskList = () => {
-    // Appelez la fonction onDelete pour supprimer la tâche
-    
     onDeleteList(idList);
     setShowDeleteModalList(false);
-  }
+  };
+
   const handleDeleteList = () => {
     setShowDeleteModalList(true);
   };
+
+  useEffect(() => {
+    localStorage.setItem(`taskList_${idList}`, JSON.stringify(tasks));
+  }, [idList, tasks]);
 
   return (
     <div className="border rounded-md shadow-md p-4 relative w-500">
       <button
         className="absolute top-0 right-0 p-2 text-gray-600 hover:text-red-600 focus:outline-none"
-        onClick={handleDeleteList} // Supprimez la première tâche pour cet exemple
+        onClick={handleDeleteList}
       >
         <FaTimes />
       </button>
@@ -51,16 +62,20 @@ const TaskList = ({onDeleteList,idList}) => {
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         onClick={() => setShowAddModal(true)}
       >
-        Ajouter une tâche 
+        Ajouter une tâche
       </button>
 
       {showAddModal && (
-        <AddTaskModal onSave={addTask} onCancel={() => setShowAddModal(false)} />
+        <AddTaskModal
+          onSave={addTask}
+          onCancel={() => setShowAddModal(false)}
+        />
       )}
+
       {showDeleteModalList && (
         <DeleteTaskModalList
-          onDeleteIList={deleteTaskList}
-          onCancelList={() => setShowDeleteModalList(false)}
+          onDeleteList={deleteTaskList}
+          onCancel={() => setShowDeleteModalList(false)}
         />
       )}
     </div>
